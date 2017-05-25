a4e8449feb6d36b0e84898c78fb005f2337d3e3b
Mostafa Khattab <mostafa.khattab@ibtikar.net.sa>
2017-05-25 13:17:26 +0200

    Initial Commit

---
+++ InstabugCodingTask/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InstabugCodingTask
+//
+//  Created by Mostafa Khattab on 5/25/17.
+//  Copyright © 2017 Mostafa Khattab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
